9e34d510e5d4f59651f67e0601a1109ed0e69d94
Bob D <txcavscout@live.com>
2022-05-15 18:24:11 -0700

    Add project files.

---
+++ Methods/ConsoleMessages.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public static class ConsoleMessages
+    {
+        public static void SayHi(string firstName)
+        {
+            Console.WriteLine($"Hello {firstName}");
+        }
+
+        public static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye!");
+            Console.WriteLine("Thank you for visiting.");
+        }
+
+        public static string GetUsersName()
+        {
+            Console.Write("Enter your first name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        public static (double, double) GetTwoNumbers()
+        {
+            bool isValid = false;
+            bool isValid2 = false;
+            double firstNumber = 1;
+            double secondNumber = 1;
+
+            while (!isValid)
+            {
+                Console.Write("Enter the first number: ");
+                isValid = double.TryParse(Console.ReadLine(), out firstNumber);
+            }
+            while (!isValid2)
+            { 
+                Console.Write("Enter the second number: ");
+                isValid2 = double.TryParse(Console.ReadLine(), out secondNumber);
+            }
+
+            return (firstNumber, secondNumber);
+
+        }
+
+        public static (string, string) GetFullName()
+        {
+            string? firstName;
+            string? lastName;
+            do
+            {
+                Console.Write("Enter your first name: ");
+                firstName = Console.ReadLine();
+            }   while (string.IsNullOrEmpty(firstName));
+
+            do
+            {
+                Console.Write("Enter your last name: ");
+                lastName = Console.ReadLine();
+            }   while (string.IsNullOrEmpty(lastName));
+             
+            return (firstName, lastName);
+        } 
+                    
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using Methods;
+
+//ConsoleMessages.SayHi();
+
+List<string> names = new();
+
+names.Add("Bob");
+names.Add("Mike");
+names.Add("Doug");
+names.Add("Mom");
+names.Add("Dad");
+names.Add("Ron");
+names.Add("Tanya");
+
+foreach (string listNames in names)
+{
+    ConsoleMessages.SayHi(listNames);
+}
+
+string name = ConsoleMessages.GetUsersName();
+ConsoleMessages.SayHi(name);
+
+
+(double firstNumber, double secondNumber) = ConsoleMessages.GetTwoNumbers();
+Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+
+(string firstName, string lastName) = ConsoleMessages.GetFullName();
+
+Console.WriteLine("\n");
+
+Console.WriteLine($"You entered {firstName} for your first name and {lastName} for your surname.");
+
+Console.ReadLine();
